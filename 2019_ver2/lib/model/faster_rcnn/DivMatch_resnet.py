7144c6e62e2b9e29ee3e81210f5b1e3419b79a9d
jrjeon <44255966+JunriJeon@users.noreply.github.com>
2019-11-26 16:51:16 +0900

    코드 오류 수정

--- 2019_ver2/lib/model/faster_rcnn/DivMatch_resnet.py
+++ 2019_ver2/lib/model/faster_rcnn/DivMatch_resnet.py
@@ -219,7 +219,7 @@ def resnet152(pretrained=False):
 
 class resnet(_fasterRCNN):
   def __init__(self, classes, num_layers=101, pretrained=False, class_agnostic=False):
-    self.model_path = 'data/pretrained_model/resnet101_caffe.pth'
+    self.model_path = 'data/pretrained_model/resnet' + str(num_layers) + '_caffe.pth'
     self.dout_base_model = 1024
     self.pretrained = pretrained
     self.class_agnostic = class_agnostic
@@ -227,8 +227,13 @@ class resnet(_fasterRCNN):
     _fasterRCNN.__init__(self, classes, class_agnostic)
 
   def _init_modules(self):
-    resnet = resnet101()
-
+    if num_layers == 50:
+      resnet = resnet50()
+    elif num_layers == 101:
+      resnet = resnet101()
+    elif num_layers == 152:
+      resnet = resnet152()
+    
     if self.pretrained == True:
       print("Loading pretrained weights from %s" %(self.model_path))
       state_dict = torch.load(self.model_path)
